d26f5e2d3fd6c9e23a98ce0c506663427ef8b512
ilman1amirul@gmail.com <ilman1amirul@gmail.com>
2021-11-06 17:23:36 +0800

    Updates

---
+++ precache-manifest.5953f6023d79455a8952bbdf596e052e.js
@@ -0,0 +1,326 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f106506018c10be1213c2ecfb2f6418",
+    "url": "/Portfolio-for-Graphic-Designer/index.html"
+  },
+  {
+    "revision": "18b671c9a8c97da182fc",
+    "url": "/Portfolio-for-Graphic-Designer/static/js/2.d253c4a2.chunk.js"
+  },
+  {
+    "revision": "5a1768750c09c8fadbc94e74a41bcc3a",
+    "url": "/Portfolio-for-Graphic-Designer/static/js/2.d253c4a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09f086825c26fd22e9f",
+    "url": "/Portfolio-for-Graphic-Designer/static/js/main.0bd07233.chunk.js"
+  },
+  {
+    "revision": "d314ec0daa9b55bed2bb",
+    "url": "/Portfolio-for-Graphic-Designer/static/js/runtime-main.f4298159.js"
+  },
+  {
+    "revision": "2989e4fc14ecbd5a7c5a094ecca9291b",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/(D) Hero.2989e4fc.JPG"
+  },
+  {
+    "revision": "5d0153e7d1613b07ffc60d97c4f0034e",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/(D1) Hero.5d0153e7.JPG"
+  },
+  {
+    "revision": "91f62ef436b0e520708cdd93bcafe874",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Felda (1).91f62ef4.png"
+  },
+  {
+    "revision": "2cd5a7904c30fcfc402ae98ec99307d9",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Graphic1.2cd5a790.png"
+  },
+  {
+    "revision": "c721aa63148dd4f4931b36ff426f8ce6",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Kolam1.c721aa63.jpg"
+  },
+  {
+    "revision": "ea131fa8a853da37fb7dd918d24025e3",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Kolam2.ea131fa8.jpg"
+  },
+  {
+    "revision": "8f01a5383282dfad66f645a0e98249d0",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Kolam3.8f01a538.jpg"
+  },
+  {
+    "revision": "635716699bbdee8498aac6a7bca7a450",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Kolam4.63571669.jpg"
+  },
+  {
+    "revision": "b34ebd1249332ba44106b4628819934e",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Kolam5.b34ebd12.png"
+  },
+  {
+    "revision": "b0cf65f1145af00ebb12d602cd2dee5f",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Mini Portfolio.b0cf65f1.pdf"
+  },
+  {
+    "revision": "0eebdfdc15877bb3e46f4948d6e071f9",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Pool1Hero.0eebdfdc.png"
+  },
+  {
+    "revision": "a757fc50b3c870a31c77affa20a5b513",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Pool2Hero.a757fc50.png"
+  },
+  {
+    "revision": "5bd56597d68e9668165d512acb6982e1",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/PoolHero3.5bd56597.png"
+  },
+  {
+    "revision": "8e7559fbea6a73a540db61b851288d91",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/UnisFrontView.8e7559fb.jpg"
+  },
+  {
+    "revision": "c5218c6721de8d30d24742a62a3ecdde",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Wedding 2.c5218c67.png"
+  },
+  {
+    "revision": "bbe4ff117f5a59985e720cd371872777",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/Wedding 3.bbe4ff11.png"
+  },
+  {
+    "revision": "2a42d257f6d7a134d9821a2af6ac0ffa",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/adobe.2a42d257.svg"
+  },
+  {
+    "revision": "69939c889d239116ceeaa168ade7dcda",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/autodesk.69939c88.svg"
+  },
+  {
+    "revision": "fad9d0e4fefd363ed610b62f33e16e28",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boat1.fad9d0e4.png"
+  },
+  {
+    "revision": "09492c4fac8dd87f860a40a6f0fc5c7e",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boat2.09492c4f.png"
+  },
+  {
+    "revision": "61506ce7c688b84e4017fc6dfca7cf6e",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boat3.61506ce7.png"
+  },
+  {
+    "revision": "9af000b4f0631d59e6c6d875747b2656",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boat4.9af000b4.png"
+  },
+  {
+    "revision": "0942796a35d4bca29083abca1c9e4087",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boatBuild.0942796a.png"
+  },
+  {
+    "revision": "c61ef5de6473435a412c1fb59007e50f",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boatDrawing.c61ef5de.png"
+  },
+  {
+    "revision": "953c4f938cb0357b435e7c29ed0b2813",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boatDrawing2.953c4f93.png"
+  },
+  {
+    "revision": "987f219ad5d2b8e32fb03bbd4dd97272",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boatHero1.987f219a.png"
+  },
+  {
+    "revision": "fc7bb54792732b88d80fee5eb7ed1fe4",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boatHero2.fc7bb547.png"
+  },
+  {
+    "revision": "4074ff03889576edf40481048ac1ae6e",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/boatMainImg.4074ff03.png"
+  },
+  {
+    "revision": "b89bde0028c2afa19cff629dae021309",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/drawingHouse1.b89bde00.png"
+  },
+  {
+    "revision": "441f2f7ff34222ed3e177cae537d7dd6",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/drawingPool.441f2f7f.PNG"
+  },
+  {
+    "revision": "61b956ba752e323aac9a5e3ac9a66492",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/drawingPool2.61b956ba.png"
+  },
+  {
+    "revision": "efca0b06f7cdebbc6f1edda3536125f0",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/drawingUnisza2.efca0b06.jpg"
+  },
+  {
+    "revision": "f0c8d394174084a5df0d5f83a003f78b",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/drone.f0c8d394.png"
+  },
+  {
+    "revision": "c3f0cfc66f38acc9ca8fcc21e450272b",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/drone2.c3f0cfc6.png"
+  },
+  {
+    "revision": "bfe1f39afca69a20113697500a03fa9a",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/droneHero1.bfe1f39a.png"
+  },
+  {
+    "revision": "ba6f79018a6d831512c62763e5bc1f7b",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/droneHero2.ba6f7901.png"
+  },
+  {
+    "revision": "eef09fab7979123db80e7acf6c2862f1",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/droneHero3.eef09fab.png"
+  },
+  {
+    "revision": "0c49cc65e3d1fde8ebc5f6921d4afb25",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/droneHero4.0c49cc65.png"
+  },
+  {
+    "revision": "e6d46ea3730a9d7a2f808242eed3e2b7",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/droneSnip.e6d46ea3.png"
+  },
+  {
+    "revision": "7e10e106c3b23e175c911aa8e98b3f11",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/droneTable.7e10e106.png"
+  },
+  {
+    "revision": "c0741a5e01ce19c04f0e7639fe6d18b8",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/facebook.c0741a5e.svg"
+  },
+  {
+    "revision": "7ee7b909abbf2be80af78920d222929a",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/houseHero.7ee7b909.png"
+  },
+  {
+    "revision": "d834e89c2de2b34d23294710963c5875",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/insta.d834e89c.svg"
+  },
+  {
+    "revision": "396cb9d5f388d3280a73abf3b46eefb3",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/jusmill.396cb9d5.png"
+  },
+  {
+    "revision": "27715cd2282c834322dfc3f26dfe04c2",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/lumion.27715cd2.svg"
+  },
+  {
+    "revision": "98965a7d0c9a293355448282725f6127",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/modernHouseMain.98965a7d.jpg"
+  },
+  {
+    "revision": "acf174d0d4c5a305e47a2822f097dcd5",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/money.acf174d0.svg"
+  },
+  {
+    "revision": "bb5a5e84c41d6f2e8905309b376cee39",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/pool1MainImg.bb5a5e84.png"
+  },
+  {
+    "revision": "c5acd055addc42c0e8aa597d16cc4e4d",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/pool1design.c5acd055.png"
+  },
+  {
+    "revision": "c3da9b9f898f7dd4671d959ff1d0585b",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/pool1design2.c3da9b9f.png"
+  },
+  {
+    "revision": "291606479974f00dc858acd3b42aa923",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/pool1design3.29160647.png"
+  },
+  {
+    "revision": "c3da9b9f898f7dd4671d959ff1d0585b",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/pool1design4.c3da9b9f.png"
+  },
+  {
+    "revision": "8e20b2a2178f0e16796b93db66c485c2",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/pool2drawing.8e20b2a2.png"
+  },
+  {
+    "revision": "3f31ac7f23fde1445592e36a6af72f4d",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/pool2second.3f31ac7f.png"
+  },
+  {
+    "revision": "1fc3491fc1e6506207d4522b2c10ab86",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/poolHero.1fc3491f.jpg"
+  },
+  {
+    "revision": "345ad24f3c78f4c521888f6df2a894bf",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/profilePic2.345ad24f.png"
+  },
+  {
+    "revision": "6938cdb5591be51e70f213e52716316a",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/sketchup.6938cdb5.svg"
+  },
+  {
+    "revision": "2a836d5a210aecc3c6a1510e26269e5b",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/solidworks.2a836d5a.svg"
+  },
+  {
+    "revision": "62da62b39a0395d5805e18c601ecd995",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/unisBackView.62da62b3.jpg"
+  },
+  {
+    "revision": "6e4f4ac371ee012ad224a9660f7b1db1",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/unisFrontView2.6e4f4ac3.jpg"
+  },
+  {
+    "revision": "07a31769cd7abad06ae4914e02b6a816",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/unisInterior1.07a31769.jpg"
+  },
+  {
+    "revision": "730459a833bc3c659597ccdcab62b40c",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/unisInterior2.730459a8.jpg"
+  },
+  {
+    "revision": "0d5308a03d880deeebffe554cdb46ba6",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/unisLayout1.0d5308a0.png"
+  },
+  {
+    "revision": "cf134b0cff47dec923f7f80779c9a8e5",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/unisLayout2.cf134b0c.png"
+  },
+  {
+    "revision": "37184d3c9a8de2aad14d53bd879e4a25",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/unisTopView.37184d3c.jpg"
+  },
+  {
+    "revision": "da1e2f366ff9a0d756b085b8997fe9b7",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaFrontView.da1e2f36.jpg"
+  },
+  {
+    "revision": "182c9f8522ecc1d5bef475e3948fa280",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaFrontView2.182c9f85.jpg"
+  },
+  {
+    "revision": "1d52aee67ea87bf1297aaf6b6c2a1de0",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaHero.1d52aee6.png"
+  },
+  {
+    "revision": "f4d9da65278702221ce745f4e8227934",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaHero2.f4d9da65.jpg"
+  },
+  {
+    "revision": "decc9f2711d1b752b2352f20dfffff8e",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaLayout.decc9f27.png"
+  },
+  {
+    "revision": "80f64a9455591410be0847050ebee89d",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaLayout2.80f64a94.png"
+  },
+  {
+    "revision": "e42557e05ba07fd3e225f0ba178020c9",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaLayout3.e42557e0.png"
+  },
+  {
+    "revision": "3f622c8cbb2077342d82c6690b958692",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaLayout4.3f622c8c.png"
+  },
+  {
+    "revision": "088a6965a15d6829b0423160a0e3629e",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaLayout5.088a6965.png"
+  },
+  {
+    "revision": "8cf199d1f380625f621ae602137552bb",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/uniszaLayout6.8cf199d1.png"
+  },
+  {
+    "revision": "4a9572feb5c96b82fc1346ca3fa893d5",
+    "url": "/Portfolio-for-Graphic-Designer/static/media/whatsapp.4a9572fe.svg"
+  }
+]);
